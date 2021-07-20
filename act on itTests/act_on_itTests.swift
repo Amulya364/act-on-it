734315651d40593dfd89d8869c612e638e1c7bc4
amulya <amulya30manoj@gmail.com>
2021-07-20 13:24:29 -0700

    Initial Commit

---
+++ act on itTests/act_on_itTests.swift
@@ -0,0 +1,33 @@
+//
+//  act_on_itTests.swift
+//  act on itTests
+//
+//  Created by amulya manoj on 7/20/21.
+//
+
+import XCTest
+@testable import act_on_it
+
+class act_on_itTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
